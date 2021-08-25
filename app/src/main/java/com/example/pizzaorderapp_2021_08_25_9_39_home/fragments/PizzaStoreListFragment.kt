e981b86995c43eb94f80bf3138f89bdf03363bbf
dqwdd <dqwdd@naver.com>
2021-08-25 14:49:19 +0900

    리스트뷰 아이템 클릭시 클릭된 가게 들고 상세 화면으로 이동 -받아서 이동하는 부분 x, 실데이터 x 아직 만들기만  : Serializable이거도 아직 추가 안함

--- app/src/main/java/com/example/pizzaorderapp_2021_08_25_9_39_home/fragments/PizzaStoreListFragment.kt
+++ app/src/main/java/com/example/pizzaorderapp_2021_08_25_9_39_home/fragments/PizzaStoreListFragment.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.pizzaorderapp_2021_08_25_9_39_home.R
+import com.example.pizzaorderapp_2021_08_25_9_39_home.ViewStoreDetailActivity
 import com.example.pizzaorderapp_2021_08_25_9_39_home.adapters.PizzaStoreAdapter
 import com.example.pizzaorderapp_2021_08_25_9_39_home.datas.StoreData
 import kotlinx.android.synthetic.main.fragment_pizza_store_list.*
@@ -20,15 +21,6 @@ class PizzaStoreListFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?, savedInstanceState: Bundle?) : View?{
-
-
-//        pizzaStoreListView.setOnItemClickListener { adapterView, view, position, l ->
-//            val clickedRoom = mPizzaStors[ position ]
-//            val myIntent = Intent(mContext, ViewStoreDetailActivity::class.java)
-//            myIntent.putExtra("roomData", clickedRoom)
-//            startActivity(myIntent)
-//        }
-
 
         return inflater.inflate(R.layout.fragment_pizza_store_list, container, false)
     }
@@ -44,6 +36,14 @@ class PizzaStoreListFragment : Fragment() {
 
         mPizzaStoreAdapter = PizzaStoreAdapter(requireContext(), R.layout.store_list_item, mPizzaStores)
         pizzaStoreListView.adapter = mPizzaStoreAdapter
+
+        pizzaStoreListView.setOnItemClickListener { adapterView, view, position, l ->
+            val clickedRoom = mPizzaStores[ position ]
+            val myIntent = Intent(requireContext(), ViewStoreDetailActivity::class.java)
+            myIntent.putExtra("store", clickedRoom)
+            startActivity(myIntent)
+//            putExtra 빨간 에러 안사라지면  : Serializable 하자
+        }
 
     }
 }
